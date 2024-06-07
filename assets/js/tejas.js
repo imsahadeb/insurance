d40e4d930baefe1b5198975fdc5c7a93bd4700f3
sahadeb <saha.ptc.om@gmail.com>
2024-06-08 00:53:12 +0530

    initial

--- assets/js/tejas.js
+++ assets/js/tejas.js
@@ -1079,3 +1079,14 @@
 
 
 })(jQuery);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(item => {
+        item.addEventListener("click", () => {
+            const answer = item.querySelector("p");
+            answer.style.display = answer.style.display === "block" ? "none" : "block";
+        });
+    });
+});
